1f6a4bd05bde165d4805a9b1f2cf168dbf3d8321
Satishpokala124 <satishpokala124@gmail.com>
2021-10-10 14:50:32 +0530

    Added basic comparision between bubbleSort and mergeSort

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import random
+import time
 from algorithms import *
 
-arr = list(map(int,input().split()))
+# n = int(input("Enter a number : "))
+# arr = list(map(int,input().split()))
+n = 1000
+while n <= 10000:
+  print('n = ', n, ":")
+  arr = [random.randint(1, n) for _ in range(n)]
+  start = time.time()
+  bubbleSort(arr)
+  start1 = time.time()
+  mergeSort(arr)
+  end = time.time()
 
-print(mergeSort(arr))
+  print('Total time taken by "bubbleSort" is', start1 - start)
+  print('Total time taken by "mergeSort" is', end - start1)
+  n = n*2
+
